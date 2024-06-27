67551856a70ef4143d3610214e1441a08cee14e7
hardik-rl <hardik.radicalloop@gmail.com>
2024-06-27 21:54:56 +0530

    common component add

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import UsersList from "./component/users/UsersList";
-
+import Header from "./shared/component/Header";
 function App() {
   return (
     <div>
-      <UsersList />
+      <Header />
+      <main className="m-5">
+        <UsersList />
+      </main>
     </div>
   );
 }
--- frontend/src/component/users/UsersList.js
+++ frontend/src/component/users/UsersList.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import CommonTable from "../Table";
 import { getAllUsers } from "./api";
+import Button from "react-bootstrap/esm/Button";
 
 const UsersList = () => {
   const [data, setData] = useState([]);
@@ -16,6 +17,11 @@ const UsersList = () => {
   }, []);
   return (
     <>
+      <div className="text-end">
+        <Button className="mb-4 ml-auto" variant="primary">
+           Add User
+        </Button>
+      </div>
       <div className="overflow-x-auto">
         <CommonTable data={data} />
       </div>
